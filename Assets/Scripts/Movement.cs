89e9e99f010e0fa35c7a5016460b6753294d0f4b
ChebHed <hedy_12@hotmail.fr>
2023-11-19 20:47:05 +0100

    rocketchanged

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,27 +4,43 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    
+    Rigidbody rb;
     // Start is called before the first frame update
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 1f;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         ProcessThrust();
+        ProcessRotation();
+
     }
 
     void ProcessThrust(){
         if(Input.GetKey(KeyCode.Space)){
-            Debug.Log("Space pressed");
+            rb.AddRelativeForce(Vector3.up * Time.deltaTime * mainThrust);
         }
-        if(Input.GetKey(KeyCode.Q)){
-            Debug.Log("Rotating Left");
+    }
+
+    void ProcessRotation(){
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
         }
-        else if(Input.GetKey(KeyCode.A)){
-            Debug.Log("Rotating Right");
+        else if(Input.GetKey(KeyCode.D)){
+            ApplyRotation(-rotationThrust);
         }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotationThisFrame);
     }
 }

e0aa14516577ba58d068a1cbef0c0ffdf4e77c16
ChebHed <hedy_12@hotmail.fr>
2023-11-20 20:30:20 +0100

    particle thrust

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    
-    Rigidbody rb;
-    AudioSource myAudioSource;
-
-    // Start is called before the first frame update
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rotationThrust = 1f;
     [SerializeField] AudioClip mainEngine;
+    [SerializeField] ParticleSystem thrustParticle;
+
+    
+    Rigidbody rb;
+    AudioSource myAudioSource;
 
     void Start()
     {
@@ -31,6 +31,7 @@ public class Movement : MonoBehaviour
     void ProcessThrust(){
         if(Input.GetKey(KeyCode.Space)){
             rb.AddRelativeForce(Vector3.up * Time.deltaTime * mainThrust);
+            thrustParticle.Play();
             if(!myAudioSource.isPlaying)
             {
                 myAudioSource.PlayOneShot(mainEngine);
